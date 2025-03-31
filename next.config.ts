a69d168e3602e0ec27deef76852c6f048d0cfb67
Sandip Bhalodiya <bhalodia.sandip@gmail.com>
2025-03-31 16:38:32 +0530

    remove local switcher

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
  
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true
+    },
+};
  
 const withNextIntl = createNextIntlPlugin();
 export default withFlowbiteReact(withNextIntl(nextConfig));
